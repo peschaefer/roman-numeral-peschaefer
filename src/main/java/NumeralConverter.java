efb3fb44888440e1961f56a7558a6a4d98ed36e9
peschaefer <peschaefer@bsu.edu>
2021-10-17 17:33:25 -0400

    Added convertToInteger method and convertIToOneTest
    Created convertToInteger method in NumeralConverter and the associated test meant to ensure sending I returns 1.

--- src/main/java/NumeralConverter.java
+++ src/main/java/NumeralConverter.java
@@ -1,2 +1,6 @@
 public class NumeralConverter {
+
+    public int convertToInteger(String romanNumeral) {
+        return 1;
+    }
 }
--- src/test/java/NumeralConverterTest.java
+++ src/test/java/NumeralConverterTest.java
@@ -1,2 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class NumeralConverterTest {
+    @Test
+    public void convertIToOneTest(){
+        NumeralConverter numeralConverter = new NumeralConverter();
+        Assertions.assertEquals(numeralConverter.convertToInteger("I"),1);
+    }
 }
